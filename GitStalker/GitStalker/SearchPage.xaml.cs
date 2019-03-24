4464dc148496885ebd71fd478d05fb6e6515e75c
Jesper Baltzersen <jesperbaltzersen@Johnnys-MacBook-Pro-2.local>
2019-03-24 10:12:55 +0100

    init add proj

---
+++ GitStalker/GitStalker/SearchPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GitStalker
+{
+    public partial class SearchPage : ContentPage
+    {
+        public SearchPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
