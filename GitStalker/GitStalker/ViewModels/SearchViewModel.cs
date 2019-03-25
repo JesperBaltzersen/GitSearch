995cb855b4cfc9d812161f9037315635bb8d90cf
jesperbaltzersen <jesperbaltzersen@hotmail.com>
2019-03-25 17:47:29 +0100

    Added SearchViewModel

---
+++ GitStalker/GitStalker/ViewModels/SearchViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using GitStalker.Models;
+using GitStalker.Services;
+using MvvmHelpers;
+using Xamarin.Forms;
+
+namespace GitStalker.ViewModels
+{
+    public class SearchViewModel : BaseViewModel
+    {
+        private readonly GitUserFetcher _gitUserFetcher;
+
+        public ObservableRangeCollection<GitUser> GitUsers { get; set; }
+        public GitUser CurrentUser { get; set; }
+
+        public SearchViewModel(GitUserFetcher gitUserFetcher)
+        {
+            GitUsers = new ObservableRangeCollection<GitUser>();
+            _gitUserFetcher = gitUserFetcher;
+            SearchGithubUserCommand = new Command<string>(
+                async (x) => await SearchGithubUser(x), 
+                (_) => !IsBusy);
+
+            //GitUsers.CollectionChanged += (sender, e) => 
+            //{
+            //    Debug.WriteLine($"Item {e.Action}"); 
+            //};
+
+
+        }
+
+        public Command<string> SearchGithubUserCommand { get;}
+        async Task SearchGithubUser(string name)
+        {
+            IsBusy = true;
+            var users = await _gitUserFetcher.GetUsersFromNameAsync(name);
+            GitUsers.AddRange(users);
+            IsBusy = false;
+
+        }   
+
+        void OnUserSelected(GitUser sender, SelectedItemChangedEventArgs e)
+        {
+            var chosenUser = e.SelectedItem as GitUser;
+            if (chosenUser != null)
+            {
+                CurrentUser = chosenUser;
+            }
+        }
+    }
+}
--- GitStalker/GitStalker/Views/SearchPage.xaml.cs
+++ GitStalker/GitStalker/Views/SearchPage.xaml.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using GitStalker.Services;
 using Xamarin.Forms;
 
 namespace GitStalker.ViewModels
 {
     public partial class SearchPage : ContentPage
     {
+
         public SearchPage()
         {
             InitializeComponent();
+
+            BindingContext = new SearchViewModel(new GitUserFetcher(new DownloadService()));
         }
     }
 }
